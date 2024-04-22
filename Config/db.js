1b74789caadae5266223c74031a3ffcc4a9dd3f2
phutadon <phutadol5627@gmail.com>
2024-04-23 02:45:40 +0700

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql'
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DBNAME || 'note_easy',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
 })
 
 connection.connect((err) => {
